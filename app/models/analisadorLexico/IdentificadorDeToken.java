e6da55d39e60375303b1b2c06496d582275e12e6
juliofarah <julio.farah@gmail.com>
2014-08-08 19:44:45 -0300

    Refactor: Refactored IdentificadorDeToken to use hashmap instead of if/else statements (Julio/Lucas)

--- app/models/analisadorLexico/IdentificadorDeToken.java
+++ app/models/analisadorLexico/IdentificadorDeToken.java
@@ -1,41 +1,30 @@
 package models.analisadorLexico;
+
+import java.util.HashMap;
+
 public class IdentificadorDeToken {
 
-    public String identifica(String token) {
-        if (token.equals("var")) {
-            return "PALAVRA_RESERVADA";
-        } else if (token.equals("String")) {
-            return "TIPO_DE_VARIAVEL";
-        } else if (token.equals("varres")) {
-            return "PALAVRA_RESERVADA";
-        } else if (token.equals("Inteiro")) {
-            return "TIPO_DE_VARIAVEL";
-        } else if (token.equals("=")) {
-            return "IGUAL";
-        } else if (token.equals(":")) {
-            return "DECLARACAO";
-        } else if (token.equals("+")) {
-            return "ADICAO";
-        } else if (token.equals("-")) {
-            return "SUBTRACAO";
-        } else if (token.equals("*")) {
-            return "MULTIPLICACAO";
-        } else if (token.equals("/")) {
-            return "DIVISAO";
-        } else if (token.equals("(")) {
-            return "PARENTESES_ABERTO";
-        } else if (token.equals(")")) {
-            return "PARENTESES_FECHADO";
-        } else if (token.equals("<>")) {
-            return "CONCATENACAO";
-        } else if (token.charAt(0) == '\"' && token.charAt(token.length()-1) == '\"') {
+	private HashMap<String, String> mapaDeTokens;
+	
+    public IdentificadorDeToken() {
+    	mapaDeTokens = new HashMap<String, String>();
+    	adicionaValores();
+	}
+
+	public String identifica(String token) {
+        if (mapaDeTokens.containsKey(token))
+        	return mapaDeTokens.get(token);
+        
+        if (tokenComecaETerminaComAspas(token)) 
             return "CONSTANTE_TIPO_STRING";
-        } else if (Character.isLetter(token.charAt(0))) {
+
+        if (tokenComecaComLetra(token)) 
             return "IDV";
-        } else if (Character.isDigit(token.charAt(0))) {
-            if(!verificaSeTodasOsCaracteresSaoNumeros(token)) return "ERRO";
-            else return "NUMERO";
-        } else return "INVALIDO";
+        
+        if (tokenComecaComNumero(token))
+            return verificaNumeros(token);
+        
+        return "INVALIDO";
     }
 
     public boolean verificaSeTodasOsCaracteresSaoNumeros(String token){
@@ -46,4 +35,42 @@ public class IdentificadorDeToken {
         }
         return true;
     }
+
+	private String verificaNumeros(String token) {
+		try{
+			Integer.valueOf(token);
+			return "NUMERO";
+		}catch (NumberFormatException e){
+			return "ERRO";
+		}
+	}
+
+	private boolean tokenComecaComNumero(String token) {
+		return Character.isDigit(token.charAt(0));
+	}
+
+	private boolean tokenComecaComLetra(String token) {
+		return Character.isLetter(token.charAt(0));
+	}
+
+	private boolean tokenComecaETerminaComAspas(String token) {
+		return token.charAt(0) == '\"' && token.charAt(token.length()-1) == '\"';
+	}
+
+    
+    private void adicionaValores() {
+		mapaDeTokens.put("var", "PALAVRA_RESERVADA");
+        mapaDeTokens.put("String", "TIPO_DE_VARIAVEL");
+        mapaDeTokens.put("varres", "PALAVRA_RESERVADA");
+        mapaDeTokens.put("Inteiro", "TIPO_DE_VARIAVEL");
+        mapaDeTokens.put("=", "IGUAL");
+        mapaDeTokens.put(":", "DECLARACAO");
+        mapaDeTokens.put("+", "ADICAO");
+        mapaDeTokens.put("-", "SUBTRACAO");
+        mapaDeTokens.put("*", "MULTIPLICACAO");
+        mapaDeTokens.put("/", "DIVISAO");
+        mapaDeTokens.put("(", "PARENTESES_ABERTO");
+        mapaDeTokens.put(")", "PARENTESES_FECHADO");
+        mapaDeTokens.put("<>", "CONCATENACAO");
+	}
 }
